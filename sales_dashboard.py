ece60721397595356b572670ed524a30886fb00d
anjali12023 <4njalisharma007@gmail.com>
2026-07-19 13:18:55 +1000

    ranking best customers

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
 order = pd.read_csv("orders.csv")
+customers = pd.read_csv("customers.csv")
+
 #parsing dates as dt objects
 order["order_date"] = pd.to_datetime(order["order_date"])
 
-
-# print(order["product_id"].to_string())
-#calculating revenue: 
-#product id
-#group by order_id
-#see product id + cost associated with it 
-#check order is not withdrawn/ cancelled
-
+#monthly revenue
 completed = order[(order["order_status"] == "Completed")]
 # print(completed)
 #sort orders by month:
 order["month"] = order.order_date.dt.to_period("M")
 monthly_revenue = order.groupby("month")["total_amount"].sum()
-print(monthly_revenue)
+# print(monthly_revenue)
+
+#Top Selling products: 
+
+
+#Best customers: 
+group = order.groupby("customer_id")
+#customer with most orders: 
+print(group["customer_id"].count().idxmax())
+#join both tables together 
+
+best_customers = (order.groupby("customer_id")["total_amount"].sum()
+                   .sort_values(ascending=False)
+                   .to_frame().merge(customers, on="customer_id"))
+print(best_customers.to_string())
